8ff7d52b60081a8751c54e32c48301c368c9f925
ahmed hussien <a.hussien87@gmail.com>
2022-04-11 23:45:41 +0200

    Fix: fixed useEffect missing dependency

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import 'normalize.css';
 import 'animate.css';
 import './App.css';
 
-const App = () => {
-  const [advice, setAdvice] = useState('No advice available for you at this moment :( ');
-  const [color, setColor] = useState();
+const initialState = {
+  advice: "",
+  color: "black",
+  animation: ""
+};
 
-  const fetchAdvice = () => {
+const App = () => {
+ 
+  const [{ advice, color, animation }, setState] = useState(initialState);
+    
+  const clearState = useCallback(() => {
+    return setState({ ...initialState });
+  }, []);
+
+  const fetchAdvice = useCallback(() => {
+
     axios.get('https://api.adviceslip.com/advice')
     .then((res)=>{
-      setAdvice(()=> {
-        const {advice} = res.data.slip;
-        return advice;
-      })
-      setColor('black')
+      
+      setState((prevState) => {
+          const {advice} = res.data.slip;
+          return ({ ...prevState, advice, color: 'black', animation :'animate__flipInX' })
+        }
+      );
+      
     })
     .catch((err) => {
-      setColor('#f64747')
+      setState((prevState) => ({ ...prevState, advice : 'No advice available for you at this moment :(', 
+      color: '#f64747', animation :'animate__pulse' }));
+
       console.log(err)
     })
-  }
+
+    clearState();
+
+  }, [clearState])
 
   useEffect(() => {
-    return fetchAdvice()
-  }, []);
+    fetchAdvice();
+  }, [fetchAdvice]);
 
   return (
     <div className='app'>
       <div className='card'>
         <div className='advice'>
-          <h1 className='heading animate__animated animate__flipInX' style={{color: color}}>
+          <h1 className={`heading animate__animated ${animation}`} style={{color: color}}>
+            { !advice.length > 0 && (
+              <span>loading...</span>
+            )}
+
             {advice}
           </h1>
           <button className='getAdvice' onClick={ () => fetchAdvice() }>Advice Me</button>
