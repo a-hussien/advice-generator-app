0996880d3d710688bb06e27f75e635e4ae204bab
ahmed hussien <a.hussien87@gmail.com>
2022-04-11 06:27:37 +0200

    first edition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'normalize.css';
+import 'animate.css';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState('No advice available for you at this moment :( ');
+  const [color, setColor] = useState();
+
+  const fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then((res)=>{
+      setAdvice(()=> {
+        const {advice} = res.data.slip;
+        return advice;
+      })
+      setColor('black')
+    })
+    .catch((err) => {
+      setColor('#f64747')
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    return fetchAdvice()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='card'>
+        <div className='advice'>
+          <h1 className='heading animate__animated animate__flipInX' style={{color: color}}>
+            {advice}
+          </h1>
+          <button className='getAdvice' onClick={ () => fetchAdvice() }>Advice Me</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
